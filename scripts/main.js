3db4053e72841167e129177b51305ede8a8ea27e
Hari Venugopalan <harivenugopalan@Haris-MacBook-Pro.local>
2018-05-12 04:39:21 -0700

    Made code more generic with single page

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,4 @@
-var origin, map, success = false, fileName = "File 1", accuracy = 0.99;
-
-window.onload = function() {
-  if(success == false)
-    document.getElementById("tcrStatus").innerHTML = "Processing of " + fileName + " failed. Please enter the location below.";
-  else {
-    document.getElementById("tcrStatus").innerHTML = "Processing of " + fileName + " succeeded, with accuracy " + accuracy +". View or modify the location below."
-    initMap("Kemper Hall UC Davis");
-    document.getElementsByClassName("location")[0].value = "Kemper Hall UC Davis";
-    addMarker("Kemper Hall UC Davis");
-  }
-
-}
+var origin, map;
 
 function initMap(location) {
   if(!location)
@@ -24,6 +12,41 @@ function initMap(location) {
     });    
   });
 }
+
+function upload() {
+  var inputTag = document.getElementById("fileUpload"), files = [];
+  if('files' in inputTag) {
+    document.getElementById("statusDiv").style.display = "block";
+    if(inputTag.files.length == 0) {
+      var footer = document.getElementById("footer");
+      document.getElementById("statusDiv").innerHTML = "Please upload one or more files.";
+      footer.style.top = window.getComputedStyle(footer).getPropertyValue('top') + 50;
+      return;
+    }
+    document.getElementById("statusDiv").innerHTML = "Processing "+inputTag.files.length+ " file(s)";
+    var spinner = document.createElement("img");
+    spinner.src = "./images/spinner.gif";
+    spinner.height = "20";
+    spinner.width = "20";
+    document.getElementById("statusDiv").appendChild(spinner);
+    document.getElementById("generalContent").innerHTML = "";
+    //makeRequest("fileUpload", inputTag.files);
+    setTimeout(function(){ loadSingleLocation({success: true, fileName: "1.pdf", accuracy: 0.99, location: "Kemper Hall UC Davis"});}, 1000);
+  }
+}
+
+function loadSingleLocation(locationObject) {
+  var generalContent = document.getElementById("generalContent");
+  document.getElementById("map").style.display = "block";
+  generalContent.innerHTML = '<div id="locationDiv"><input type="text" class="location form-control" placeholder="Enter location" /><br /><input type="button" id="locationButton" value="Visualize!" class="btn btn-default" onclick="visualize()" /></div><input type="button" id="updateButton" value="Update!" class="btn btn-default" onclick="update()" /></div>';
+  if(locationObject.success == true) {
+    document.getElementById("statusDiv").innerHTML = "Processing of " + locationObject.fileName + " succeeded, with accuracy " + locationObject.accuracy +". View or modify the location below."
+    initMap(locationObject.location);
+    document.getElementsByClassName("location")[0].value = locationObject.location;
+    addMarker(locationObject.location);
+  }
+}
+
 
 function addMarker(location) {
   var locationGeocoder = new google.maps.Geocoder(), position, marker;
@@ -43,7 +66,7 @@ function addMarker(location) {
 function visualize() {
   var location = document.getElementsByClassName("location")[0].value;
   document.getElementById("updateButton").style.display = "block";
-  document.getElementById("tcrStatus").innerHTML = location + " marked on the map.";
+  document.getElementById("statusDiv").innerHTML = location + " marked on the map.";
   initMap(location);
   addMarker(location);
 }
